963422d7fbf991c75f474d789d46078075a5c218
Chloe L <chloealarkin@outlook.com>
2019-11-05 13:04:15 -0500

    ResNet script template

---
+++ notebooks/ResNet.py
@@ -0,0 +1,31 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+#TODO: Must install PIL as "Pillow"
+
+model = torch.hub.load('pytorch/vision', 'resnet101', pretrained=True)
+model.eval()
+
+# sample execution (requires torchvision)
+
+input_image = Image.open(filename) #TODO: Change this line to load our image files into the script
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+input_tensor = preprocess(input_image)
+input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+# move the input and model to GPU for speed if available
+if torch.cuda.is_available():
+    input_batch = input_batch.to('cuda')
+    model.to('cuda')
+
+with torch.no_grad():
+    output = model(input_batch)
+# Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
+print(output[0])
+# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+print(torch.nn.functional.softmax(output[0], dim=0))
