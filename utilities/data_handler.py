163c0f088f92d80b1cbdd51d8cf4f3ec58b8f283
ianmagnusson <jackmagnusson@gmail.com>
2019-11-16 23:38:54 -0500

    added classification output to data_handler

--- utilities/data_handler.py
+++ utilities/data_handler.py
@@ -9,20 +9,32 @@ import tweepy
 from tweepy import TweepError
 import os
 import random
+import csv
 
 RANDOM_SEED = 42
 
 class DataHandler:
-    def __init__(self, unlabeled_data_filename, labeled_data_filename):
+    def __init__(self, unlabeled_data_filename, labeled_data_filename, classifications_filename):
 
         # set random seed for reproducable data sets
         random.seed(RANDOM_SEED)
+
+        # Establish dict of annotated scores for each tweet ID
+        class_encoding = {'dont_know_or_cant_judge':-1 , 'informative': 1, 'not_informative': 0}
+        self.id_to_class = {}  # this is the binary informative / not informative score
+        # CHANGE FILEPATH — Read in the classifications of each tweet
+        with open(classifications_filename) as fin:
+            reader = csv.DictReader(fin, dialect='excel - tab')
+            for line in reader:
+                self.id_to_class[line['tweet_id']]=class_encoding[line['text_info']]
 
         # load json data
         with open(unlabeled_data_filename) as fin:
             self.unlabeled = [json.loads(line) for line in fin.readlines()]
         with open(labeled_data_filename) as fin:
-            self.labeled = [json.loads(line) for line in fin.readlines()]
+            jsons = [json.loads(line) for line in fin.readlines()]
+            # skip the don't know classifications (there are only 6)
+            self.labeled = [json for json in jsons if self.id_to_class[json['user']['id']] != -1 ]
 
         # get set of labeled tweet ids
         self.labeled_tweet_ids = set()
@@ -71,11 +83,13 @@ class DataHandler:
         # grab user data until threshold
         test_labeled = []
         test_histories = []
+        test_classes = []
         cur_user_idx = 0
         while(len(test_labeled) < goal_number_of_test_tweets):
             cur_user = basket_of_users[cur_user_idx]
             labeled_tweets = self.labeled_tweets_by_user[cur_user]
             test_labeled.extend(labeled_tweets)
+            test_classes.extend([self.id_to_class[json['id']] for json in labeled_tweets])
 
             # put the users history in for each of their tweets (to keep test_history aligned with test_labeled)
             for _ in range(len(labeled_tweets)):
@@ -85,9 +99,11 @@ class DataHandler:
         # put the remaining data in the training set
         train_labeled = []
         train_histories = []
+        train_classes = []
         for cur_user in basket_of_users[cur_user_idx:]:
             labeled_tweets = self.labeled_tweets_by_user[cur_user]
             train_labeled.extend(labeled_tweets)
+            train_classes.extend([self.id_to_class[json['id']] for json in labeled_tweets])
 
             # put the users history in for each of their tweets (to keep train_history aligned with train_labeled)
             for _ in range(len(labeled_tweets)):
@@ -100,4 +116,4 @@ class DataHandler:
             if tweet_json['user']['id'] in training_users:
                 train_merged.append(tweet_json)
 
-        return train_labeled, train_histories, test_labeled, test_histories, train_merged
+        return train_labeled, train_histories, test_labeled, test_histories, train_merged, train_classes, test_classes
